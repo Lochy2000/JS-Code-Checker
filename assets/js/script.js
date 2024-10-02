829f141958652b4a275112ecc78dc62b18b3660d
Lochlann O'Higgins <164012429+Lochy2000@users.noreply.github.com>
2024-10-02 10:10:12 +0000

    added processoption, feeding back options to the API in correct format

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,23 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
+function processOption(form){
+    
+    let optArray = [];
+
+    for (let entry of form.entries()) {
+        if  (entry[0] === "options"){
+            optArray.push(entry[1]);
+        }
+    }
+    form.delete("options");
+
+    form.append("options", optArray.join());
+    return form;
+}
+
 async function postForm(e) {
-    const form = new FormData(document.getElementById("checksform"));
+    const form = processOption(new FormData(document.getElementById("checksform")));
 
     const response = await fetch(API_URL, {
         method: "POST",
@@ -19,17 +34,32 @@ async function postForm(e) {
     const data = await response.json();
 
     if (response.ok ) {
-        displayErros(data);
+        displayErrors(data);
     }else {
         throw new Error(data.error)
     }
 }
 
-function displayErros(data) {
+function displayErrors(data) {
+    let heading = `JSHint Results for ${data.file}`;
+    let results;
 
-    let heading = `JSHint Results for `
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No Errors reported!</div>`;
+    } else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.column}</span></div>`;
+            results += `<div class="error">${error.error}</div>`; 
+        }
+    }
 
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show(); 
 }
+
 
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`
